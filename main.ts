ff5a55eebc21b8bebf9299adf8cbfb0b9b29f44c
Emeraldtheporcupine <125104247+Emeraldtheporcupine@users.noreply.github.com>
2026-01-23 12:59:49 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,24 +5,6 @@ namespace SpriteKind {
     export const Claw = SpriteKind.create()
     export const TakeAwayTree = SpriteKind.create()
 }
-sprites.onDestroyed(SpriteKind.TakeAwayTree, function (sprite) {
-    Character.ax = -54
-    timer.background(function () {
-        pauseUntil(() => Character.x < 1236)
-        Character.ax = 0
-        Character.sayText("What was that?", 3000, true)
-        timer.after(3500, function () {
-            Character.sayText("I must stop them!", 2000, true)
-            timer.after(2500, function () {
-                timer.background(function () {
-                    Character.ax = 300
-                    pauseUntil(() => Character.x > 1300)
-                    Character.ax = 0
-                })
-            })
-        })
-    })
-})
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (Cutscene == false) {
         if (Character.vy == 0) {
@@ -212,6 +194,31 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`transparency16`, function (sp
             cutscene()
         })
     }
+})
+sprites.onDestroyed(SpriteKind.TakeAwayTree, function (sprite) {
+    Character.ax = -54
+    timer.background(function () {
+        pauseUntil(() => Character.x < 1236)
+        Character.ax = 0
+        Character.sayText("What was that?", 3000, true)
+        timer.after(3500, function () {
+            Direction = 1
+            Character.sayText("I must stop them!", 2000, true)
+            timer.after(2500, function () {
+                timer.background(function () {
+                    Character.ax = 250
+                    pauseUntil(() => Character.x > 1275)
+                    Character.ax = 100
+                    animation.runImageAnimation(
+                    CutSprite,
+                    assets.animation`Next Level Blackout`,
+                    100,
+                    false
+                    )
+                })
+            })
+        })
+    })
 })
 function cutscene () {
     if (CutscenePart == 1) {
